418921ebabee75a9c85ce07d52d3d8f5e4328b40
Ninsen <65847077+Ninsen@users.noreply.github.com>
2022-03-12 11:34:25 +0800

    Add files via upload
    upload init codes

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+      // dev 独有配置
+      plugins: [vue()],
+      server: {
+        host: 'localhost',
+        port: 8888,
+        open: true,
+        strictPort: false,
+        https: false,
+        // 反向代理
+        // proxy: {
+        //   '/admin': {
+        //     target: 'http://api.vite-admin.com',
+        //     changeOrigin: true,
+        //     rewrite: (path) => path.replace(/^\/admin/, '')
+        //   },
+        // }    
+
+        //proxy: {
+        //   // string shorthand
+        //   '/foo': 'http://localhost:4567/foo',
+        //   // with options
+        //   '/api': {
+        //     target: 'http://jsonplaceholder.typicode.com',
+        //     changeOrigin: true,
+        //     rewrite: (path) => path.replace(/^\/api/, '')
+        //   },
+        //   // with RegEx
+        //   '^/fallback/.*': {
+        //     target: 'http://jsonplaceholder.typicode.com',
+        //     changeOrigin: true,
+        //     rewrite: (path) => path.replace(/^\/fallback/, '')
+        //   }
+        // }
+      },
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build 独有配置
+    }
+  }
+})
